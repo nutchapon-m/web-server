a38343079cf9bcd92acbd583f0c1f242ba593ceb
nut-c <nutchapon.met1002@gmail.com>
2025-10-09 00:31:10 +0700

    add rate limit middleware

---
+++ app/sdk/mid/limiter.go
@@ -0,0 +1,24 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/nutchapon-m/web-server/app/sdk/errs"
+	"github.com/nutchapon-m/web-server/foundation/web"
+	"golang.org/x/time/rate"
+)
+
+var limiter = rate.NewLimiter(rate.Every(time.Second), 5)
+
+func Limiter() web.MidFunc {
+	return func(next web.HandlerFunc) web.HandlerFunc {
+		return func(ctx context.Context, r *http.Request) web.Encoder {
+			if err := limiter.Wait(ctx); err != nil {
+				return errs.Newf(errs.TooManyRequests, "Rate limit exceeded")
+			}
+			return next(ctx, r)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/nutchapon-m/web-server
 
 go 1.24.0
 
-require github.com/spf13/viper v1.21.0
+require (
+	github.com/spf13/viper v1.21.0
+	golang.org/x/time v0.14.0
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
